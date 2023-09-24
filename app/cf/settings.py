b33fbe04274d5131d6b0fcefffe612f5210dc87b
Swelan Auguste <swelanauguste@gmail.com>
2023-09-24 07:39:14 -0400

    logging

--- app/cf/settings.py
+++ app/cf/settings.py
@@ -199,3 +199,23 @@ REQUEST_PLUGINS = (
     "request.plugins.TopBrowsers",
     "request.plugins.ActiveUsers",
 )
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "./debug.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
