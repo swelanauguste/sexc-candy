9af7af1789cb12a8a0c1eca523b07c21014de00c
swelanauguste <swelanauguste@gmail.com>
2023-10-23 14:10:44 -0400

    slug

--- app/letters/urls.py
+++ app/letters/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     # path("", HomeView.as_view(), name="home"),
     path("", letter_list_view, name="letter-list"),
     path("create/", letter_create_view, name="letter-create"),
-    path("letter/detail/<int:slug>/", letter_detail_view, name="letter-detail"),
-    path("add/comment/<int:slug>/", letter_comment_create_view, name="add-comment"),
+    path("letter/detail/<slug:slug>/", letter_detail_view, name="letter-detail"),
+    path("add/comment/<slug:slug>/", letter_comment_create_view, name="add-comment"),
     path("create/action", ActionCreateView.as_view(), name="action-create"),
     path(
         "create/correspondence",
