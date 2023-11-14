72a0ba345e553d830429d0464efae34d0f6f5b19
swelanauguste <swelanauguste@gmail.com>
2023-11-14 10:35:53 -0400

    docker

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-# if [ "$DATABASE" = "postgres" ]
-# then
-#     echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-#     while ! nc -z $SQL_HOST $SQL_PORT; do
-#       sleep 0.1
-#     done
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
 
-#     echo "PostgreSQL started"
-# fi
+    echo "PostgreSQL started"
+fi
+
 # python manage.py flush --noinput
 python manage.py migrate
 python manage.py makemigrations
@@ -17,7 +18,7 @@ python manage.py migrate
 # python manage.py migrate django_celery_results
 
 
-# python manage.py createsuperuser --username kingship --email kingship.lc@gmail.com --noinput
+python manage.py createsuperuser --username kingship --email kingship.lc@gmail.com --noinput
 
 # python manage.py add_qualifications
 
